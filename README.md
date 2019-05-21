fcbfbc21547b7ceaac5b3455964ebfca2bd34486
vishnuharish <vishnuharish12@gmail.com>
2019-05-21 12:45:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-4dtepd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-4dtepd)
---
+++ app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+const config = {
+  apiKey: "AIzaSyCM2Dfd0hRJNTfn7RmAuc1NnoPLsp97_HU",
+  authDomain: "fir-training-61959.firebaseapp.com",
+  databaseURL: "https://fir-training-61959.firebaseio.com",
+  storageBucket: "fir-training-61959.appspot.com",
+  messagingSenderId: "1057967227504"
+};
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
+  ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
